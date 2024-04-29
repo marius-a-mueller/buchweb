47eaf9392b2045093215e966d75ea8ff87f6d804
Marius Müller <marius@bscnet.de>
2024-04-29 13:13:32 +0200

    Added very basic tabbar

---
+++ src/components/tabbar/TabBar.tsx
@@ -0,0 +1,11 @@
+import { MenuItem } from "./MenuItem"
+
+export const TabBar = () => {
+    return (
+        <div>
+            <MenuItem name="Home" />
+            <MenuItem name="About" />
+            <MenuItem name="Contact" />
+        </div>
+    )
+}
