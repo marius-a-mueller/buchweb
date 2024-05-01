afb5ebc9da2a7046823a8404185f75e4b00cc4b2
Marius Müller <marius@bscnet.de>
2024-05-01 12:46:07 +0200

    small changes with logo

--- src/components/tabbar/TabBar.tsx
+++ src/components/tabbar/TabBar.tsx
@@ -1,8 +1,9 @@
 import { AppBar, Container, Toolbar, Box } from '@mui/material';
 import { MenuItem } from './MenuItem';
 import MenuDropdown from './MenuDropdown';
-import HKALogo from './HKALogo.png';
+import HKALogo from '../../assets/HKALogo.png';
 import Login from './Login';
+import { Link } from 'react-router-dom';
 
 export const TabBar = () => {
   return (
@@ -10,11 +11,13 @@ export const TabBar = () => {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-            <img
-              src={HKALogo}
-              alt="HKA Logo"
-              style={{ width: '200px', marginRight: '20px' }}
-            />
+            <Link to="/">
+              <img
+                src={HKALogo}
+                alt="HKA Logo"
+                style={{ width: '200px', marginRight: '20px' }}
+              />
+            </Link>
             <MenuItem name="Suche" link="search" />
             <MenuItem name="Neues Buch" link="new" />
             <MenuDropdown />
