6d9353740575745fadb9a05cdc5e8bfb9e8899ea
Yasin Arslan <yasinars5804@gmail.com>
2024-05-01 12:15:29 +0200

    Formatierungsfehler

--- src/components/tabbar/TabBar.tsx
+++ src/components/tabbar/TabBar.tsx
@@ -2,8 +2,6 @@ import { AppBar, Container, Toolbar } from '@mui/material';
 import { MenuItem } from './MenuItem';
 import BookIcon from '@mui/icons-material/Book';
 import MenuDropdown from './MenuDropdown';
-
-// Importiere dein Bild
 import HKALogo from './HKALogo.png';
 
 export const TabBar = () => {
