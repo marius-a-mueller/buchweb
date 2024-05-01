0cb5cc14f8f7bc91c0cb099315dbd5c05d8bd6a6
grso1012 <grimpe.soulaiman@gmail.com>
2024-05-01 12:09:41 +0200

    Login-Komponente

---
+++ src/components/tabbar/Login.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+
+export default function Login() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="secondary" onClick={handleClickOpen}>
+        Login
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Login</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Bitte geben Sie Ihre Login-Daten ein, um fortzufahren.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email-Adresse"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            margin="dense"
+            id="password"
+            label="Passwort"
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Abbrechen</Button>
+          <Button onClick={handleClose}>Login</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
