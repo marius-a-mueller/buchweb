71a4cc445953bc497715e8ecf8288c82e9d36e1d
grso1012 <grimpe.soulaiman@gmail.com>
2024-05-27 12:16:25 +0200

    Reverted the merge branch from the previous commit (wurde etwas hinzugefügt beim comitten der Änderungsdetails)

--- src/util/AxiosInstance.ts
+++ src/util/AxiosInstance.ts
@@ -1,16 +1,7 @@
 import axios from 'axios';
-import cert from '@/assets/certificate.crt?raw';
-import { Agent } from 'node:https';
-
-const httpsAgent = new Agent({
-  requestCert: true,
-  rejectUnauthorized: false,
-  ca: cert,
-});
 
 const AxiosInstance = axios.create({
   baseURL: 'https://localhost:3000',
-  httpsAgent,
   timeout: 1000,
 });
 
