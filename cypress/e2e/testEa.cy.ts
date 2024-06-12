bc0b8dfdafe0b4b2e98914c0bf3752617dd490c7
Yasin Arslan <yasinars5804@gmail.com>
2024-06-12 09:29:44 +0200

    Cypress New Book

--- cypress/e2e/testEa.cy.ts
+++ cypress/e2e/testEa.cy.ts
@@ -10,7 +10,6 @@ describe('template spec', () => {
     cy.get('[data-cy=login-button-second]').click();
     cy.wait(2000);
     cy.get('[data-cy=logo').should('exist');
-
   });
   it('Überprüfe Default Zustand', () => {
     cy.get('[data-cy=login-button]').should('exist');
@@ -24,7 +23,7 @@ describe('template spec', () => {
     cy.intercept('GET', '**/rest/**').as('searchRequest');
     cy.get('[data-cy=Suche').click();
     cy.wait(2000);
-    cy.get('[data-cy=isbn').type('978-3-897-22583-1');
+    cy.get('[data-cy=isbn-search').type('978-3-897-22583-1');
     cy.get('[data-cy=search-button-form').click();
     cy.wait('@searchRequest')
       .its('response.body')
@@ -37,5 +36,25 @@ describe('template spec', () => {
     cy.get('[data-cy=Säulendiagramm').click();
     cy.get('[data-cy=Diagramme').click();
     cy.get('[data-cy=Piechart').click();
+  });
+  it('Lege neues Buch an', () => {
+    cy.get('[data-cy=login-button]').click();
+    cy.get('[data-cy=login-username').type('admin');
+    cy.get('[data-cy=login-password').type('p');
+    cy.get('[data-cy=login-button-second]').click();
+    cy.wait(2000);
+    cy.intercept('POST', '**/rest/**').as('newBookRequest');
+    cy.get('[data-cy="Neues Buch"]').click();
+    cy.wait(2000);
+    cy.get('[data-cy=isbn-post').type('978-3-649-64224-4');
+    cy.wait(2000);
+    cy.get('[data-cy=titel-post').type('Dracula');
+    cy.wait(2000);
+    cy.get('[data-cy=homepage-post').type('https://Dracula.dev');
+    cy.wait(2000);
+    cy.get('[data-cy=type]').contains('KINDLE').click();
+    cy.wait(2000);
+    cy.get('[data-cy=post-button-form').click();
+    cy.wait(4000);
   });
 });
