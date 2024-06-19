faa445efaf9058e932b066c5594f722b941f30f3
grso1012 <grimpe.soulaiman@gmail.com>
2024-06-19 11:54:04 +0200

    Fixed Test in Cypress

--- cypress/e2e/testEa.cy.ts
+++ cypress/e2e/testEa.cy.ts
@@ -1,9 +1,12 @@
+
+
 describe('template spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:4200/');
   });
 
   it('Überprüfe Login', () => {
+    cy.get('[data-cy=logo]').click();
     cy.get('[data-cy=login-button]').click();
     cy.get('[data-cy=login-username').type('admin');
     cy.get('[data-cy=login-password').type('p');
@@ -16,6 +19,7 @@ describe('template spec', () => {
     cy.get('[data-cy=logo').should('exist');
     cy.get('[data-cy=Suche').should('exist');
     cy.get('[data-cy=neuesBuch').should('not.exist');
+    cy.get('[data-cy=logo').click();
     cy.get('[data-cy=login-button]').click();
     cy.get('[data-cy=login-username').type('admin');
     cy.get('[data-cy=login-password').type('p');
@@ -26,6 +30,7 @@ describe('template spec', () => {
     cy.get('[data-cy=mehrDazu_Suche').should('exist');
   });
   it('Überprüfe Suche mit Gegebene ISBN', () => {
+    cy.get('[data-cy=logo]').click();
     cy.intercept('GET', '**/rest/**').as('searchRequest');
     cy.get('[data-cy=Suche').click();
     cy.wait(2000);
@@ -38,6 +43,7 @@ describe('template spec', () => {
       });
   });
   it('Lege neues Buch an', () => {
+    cy.get('[data-cy=logo]').click();
     cy.get('[data-cy=login-button]').click();
     cy.get('[data-cy=login-username').type('admin');
     cy.get('[data-cy=login-password').type('p');
@@ -58,40 +64,21 @@ describe('template spec', () => {
     cy.wait(4000);
   });
   it('Überprüfe die Navigation der Diagramme', () => {
+    cy.get('[data-cy=logo]').click();
     cy.get('[data-cy=Diagramme').click();
     cy.get('[data-cy=Säulendiagramm').click();
     cy.get('[data-cy=Diagramme').click();
     cy.get('[data-cy=Piechart').click();
   });
-  it('Überprüfen der Sidebar', () => {
-    cy.get('[data-cy=menuButton').should('exist');
-    cy.get('[data-cy=menuButton').click();
-    cy.get('[data-cy=SucheSide').should('exist');
-    cy.intercept('GET', '**/rest/**').as('searchRequest');
-    cy.get('[data-cy=SucheSide').click();
-    cy.wait(2000);
-    cy.get('[data-cy=menuButton').click()
-    //cy.get('[data-cy=neuesBuchSide').should('not.exist');
-    cy.get('[data-cy=NeuesBuchSide').click();
-    cy.get('[data-cy=login-button]').click();
-    cy.get('[data-cy=login-username').type('admin');
-    cy.get('[data-cy=login-password').type('p');
-    cy.get('[data-cy=login-button-second]').click();
-    cy.wait(2000);
-    cy.get('[data-cy=menuButton').click()
-    cy.get('[data-cy=DiagrammeS').click();
-    cy.get('[data-cy=SäulendiagrammS').click();
-    cy.wait(2000);
-    cy.get('[data-cy=DiagrammeS').click();
-    cy.get('[data-cy=KuchendiagrammS').click();
-  });
-
+  
   it('Überprüfe Dark Mode', () => { 
+    cy.get('[data-cy=logo]').click();
     cy.get('[data-cy=toggle-dark-mode').click();
     cy.get('[data-cy=toggle-dark-mode').click();
   });
 
   it('Überprüfe Logout', () => {
+    cy.get('[data-cy=logo]').click();
     cy.get('[data-cy=login-button]').click();
     cy.get('[data-cy=login-username').type('admin');
     cy.get('[data-cy=login-password').type('p');
@@ -99,5 +86,49 @@ describe('template spec', () => {
     cy.wait(2000);
     cy.get('[data-cy=logout-button').click();
     cy.get('[data-cy=login-button]').should('exist');
+  });
+
+  it('Überprüfen der Sidebar', () => {
+    cy.viewport('iphone-xr');
+
+    cy.get('[data-cy=logo]').click();
+    cy.get('[data-cy=menuButton').should('exist');
+    cy.get('[data-cy=menuButton').click();
+    cy.get('[data-cy=login-button]').click();
+    cy.get('[data-cy=login-username').type('admin');
+    cy.get('[data-cy=login-password').type('p');
+    cy.get('[data-cy=login-button-second]').click();
+    cy.wait(2000);
+    cy.get('[data-cy=SucheSide').should('exist');
+    cy.intercept('GET', '**/rest/**').as('searchRequest');
+    cy.get('[data-cy=SucheSide').click();
+    cy.wait(2000);
+    cy.get('[data-cy=isbn-search').type('978-3-897-22583-1');
+    cy.get('[data-cy=search-button-form').click();
+    cy.wait('@searchRequest')
+      .its('response.body')
+      .should((body) => {
+        expect(body._embedded.buecher[0].isbn).to.equal('978-3-897-22583-1');
+      });
+    cy.get('[data-cy=menuButton').click()
+    //cy.get('[data-cy=neuesBuchSide').should('not.exist');
+    cy.get('[data-cy=NeuesBuchSide').click();
+    cy.wait(2000);
+    cy.get('[data-cy=isbn-post').type('978-3-649-64224-7');
+    cy.wait(2000);
+    cy.get('[data-cy=titel-post').type('Frankenstein');
+    cy.wait(2000);
+    cy.get('[data-cy=homepage-post').type('https://Frankenstein.dev');
+    cy.wait(2000);
+    cy.get('[data-cy=type]').contains('KINDLE').click();
+    cy.wait(2000);
+    cy.get('[data-cy=post-button-form').click();
+    cy.wait(4000);
+    cy.get('[data-cy=menuButton').click()
+    cy.get('[data-cy=DiagrammeS').click();
+    cy.get('[data-cy=SäulendiagrammS').click();
+    cy.wait(2000);
+    cy.get('[data-cy=DiagrammeS').click();
+    cy.get('[data-cy=KuchendiagrammS').click();
   });
 });
