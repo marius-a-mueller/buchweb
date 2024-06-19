ef28a019e768694d3f01fe2773b4c95131f25ebc
Marius Müller <marius@bscnet.de>
2024-06-19 23:13:26 +0200

    Allow self signed ssl in axios

--- src/util/AxiosInstance.ts
+++ src/util/AxiosInstance.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
+import https from 'https';
 
 const AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   timeout: 5000,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
 });
 
 export { AxiosInstance };
