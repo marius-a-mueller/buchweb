78093fd93ecfdb64da224781ca827bca3dcdf670
Marius Müller <marius@bscnet.de>
2024-06-20 00:18:16 +0200

    new fix iteration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
