700ecff61cb599b5a8f5e66608c6d884a2fe49c5
Marius Müller <marius@bscnet.de>
2024-06-20 00:11:13 +0200

    fix attempt ssl cert

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './features/auth';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import https from 'https';
+
+https.globalAgent.options.rejectUnauthorized = false;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
