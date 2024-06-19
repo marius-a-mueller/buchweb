3d65da14dd11b39afa996fcade9e85a534aaba9a
Marius Müller <marius@bscnet.de>
2024-06-20 00:05:26 +0200

    Fixed browser compability issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   base: '/buchweb/',
   server: {
     port: 4200,
@@ -12,6 +13,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      https: 'https-browserify',
     },
   },
 });
