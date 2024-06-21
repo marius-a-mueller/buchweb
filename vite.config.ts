197302a297b213e6f7407e8e82da2c8e8fc15577
Marius Müller <marius@bscnet.de>
2024-06-21 20:11:26 +0200

    Removing unnecessary code

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
   base: '/buchweb/',
   server: {
     port: 4200,
@@ -15,7 +14,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      https: 'https-browserify',
     },
   },
 });
