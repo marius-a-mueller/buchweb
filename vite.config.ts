a0d90a7c8dce90b73920572c5c62aa0b0fb295ab
Marius Müller <marius@bscnet.de>
2024-06-25 12:20:02 +0200

    added public dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/buchweb/',
-  server: {
-    port: 4200,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    base: '/buchweb/',
+    publicDir: 'public',
+    server: {
+        port: 4200,
     },
-  },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });
